2a4544c0bc607db34663059adb841547fb9621a1
Vincent Jacquemet <vincent.jacquemet@umontreal.ca>
2023-10-07 16:22:02 -0400

    fix meta-data

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 
 NAME = "surfacetopology"
-VERSION = "0.1.0"
+VERSION = "0.1.2"
 DESCR = "Check the validity and determine the topology of a triangulated surface"
+KEYWORDS = "triangular,mesh,surface,manifold,boundary,orientability,genus"
 URL = "https://github.com/jacquemv/surfacetopology"
 REQUIRES = ['numpy', 'cython']
 
@@ -15,13 +16,15 @@ LICENSE = "MIT"
 SRC_DIR = "surfacetopology"
 PACKAGES = [SRC_DIR]
 
-ext = Extension("topology",
-                sources=[SRC_DIR + "/trisurftopology.cpp", SRC_DIR + "/topology.pyx"],
+ext = Extension(SRC_DIR + ".topology",
+                sources=[SRC_DIR + "/trisurftopology.cpp", 
+                         SRC_DIR + "/topology.pyx"],
                 libraries=[],
                 extra_compile_args=['-O3'],
                 extra_link_args=[],
                 language="c++",
-                include_dirs=[SRC_DIR])
+                include_dirs=[SRC_DIR], )
+ext.cython_directives = {'language_level': "3"}
 
 EXTENSIONS = [ext]
 
@@ -31,6 +34,9 @@ setup(install_requires=REQUIRES,
       name=NAME,
       version=VERSION,
       description=DESCR,
+      keywords=KEYWORDS,
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type='text/markdown',
       author=AUTHOR,
       author_email=EMAIL,
       url=URL,
